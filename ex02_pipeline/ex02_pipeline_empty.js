c781e521aa7229684ede730a05304675809bcc27
calvindo <calvindelloro@mail.de>
2022-03-29 17:14:26 +0200

    29.03.2022

--- ex02_pipeline/ex02_pipeline_empty.js
+++ ex02_pipeline/ex02_pipeline_empty.js
@@ -50,13 +50,13 @@ function main() {
 
     const twoTrianglesColors = [
         //front triangle
-        1.0,0.0,0.0,1.0,
-        0.0,1.0,0.0,1.0,
-        0.0,0.0,1.0,1.0,
+        1.0, 0.0, 0.0, 1.0,
+        0.0, 1.0, 0.0, 1.0,
+        0.0, 0.0, 1.0, 1.0,
         //back triangle
-        1.0,1.0,0.0,1.0,
-        1.0,.0,0.5,1.0,
-        1.0,0.53,0.75,1.0   
+        1.0, 1.0, 0.0, 1.0,
+        1.0, .0, 0.5, 1.0,
+        1.0, 0.53, 0.75, 1.0
     ]
 
     /*====== Define color buffer ======*/
@@ -67,12 +67,26 @@ function main() {
 
     /*====== Define shader sources ======*/
     const vsSource = `
-            
+            attribute vec4 aPosition;
+            attribute vec4 aColor;
+            uniform mat4 uModelViewMatrix;
+            uniform mat4 uProjectionMatrix;
+            varying vec4 vFragColor;
+
+            void main(){
+                gl_Position = uProjectionMatrix * uModelViewMatrix * aPosition;
+                vFragColor = aColor;
+            }
     `;
 
     const fsSource = `
         precision mediump float;
         
+        varying vec4 vFragColor;
+
+        void main(){
+            gl_FragColor = vFragColor;
+        }
             
     `;
 
@@ -83,13 +97,46 @@ function main() {
     /* TODO ====== Create and link shader programs ======*/
 
     /*========== Connect the attributes with the vertex shader ===================*/
+    const posAttribLocation = gl.getAttribLocation(program, "aPosition");
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.vertexAttribPointer(posAttribLocation, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(posAttribLocation);
+
+
+    const colorAttribLocation = gl.getAttribLocation(program, "aColor");
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.vertexAttribPointer(colorAttribLocation, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(colorAttribLocation);
 
     /*========== Connect the uniforms with the vertex shader ===================*/
 
+
+    const projMatrixLocation = gl.getUniform(program, "uProjectionMatrix");
+    
     // define projection matrix
+    
+    const projectionMatrix = mat4.create();
+    const fieldOfView = 45 * Math.PI / 180
+    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+    const zNear = 0.1;
+    const zFar = 100.0;
+    mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar);
+    mat4.translate(projectionMatrix, projectionMatrix, [1.0, 0.0, 0.0]);
+    console.log("Projection matrix: %s", mat4.str(projectionMatrix));
 
+    gl.uniformMatrix4fv(projMatrixLocation, false, projectionMatrix);
+    
     // define modelview matrix
-
+    
     /*========== Drawing ======================== */
+
+    gl.clearColor(1, 1, 1, 1);
+    // TODO
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    const mode = gl.TRIANGLES;
+    const first = 0;
+    const count = 6;
+    gl.drawArrays(mode, first, count)
 
 } // be sure to close the main function with a curly brace.
